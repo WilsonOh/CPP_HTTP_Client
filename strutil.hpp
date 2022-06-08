26ca79cab1eaf9cebf64c7d1328c9171703562b2
WilsonOh <oh.wilson123@gmail.com>
2022-06-08 17:04:09 +0800

    added 'trim' function

--- strutil.hpp
+++ strutil.hpp
@@ -3,11 +3,13 @@
 
 #include <algorithm>
 #include <cctype>
+#include <functional>
 #include <string>
 #include <vector>
 
 namespace strutil {
-inline std::vector<std::string> split(std::string s, std::string delimiter) {
+inline std::vector<std::string> split(const std::string &s,
+                                      const std::string &delimiter) {
   size_t pos_start = 0;
   size_t pos_end;
   size_t delim_len = delimiter.length();
@@ -17,12 +19,12 @@ inline std::vector<std::string> split(std::string s, std::string delimiter) {
   while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
     token = s.substr(pos_start, pos_end - pos_start);
     pos_start = pos_end + delim_len;
-    res.push_back(token);
+    res.emplace_back(token);
   }
 
   std::string last = s.substr(pos_start);
   if (!last.empty())
-    res.push_back(s.substr(pos_start));
+    res.emplace_back(last);
   return res;
 }
 
@@ -33,6 +35,29 @@ inline std::string lowers(const std::string &s) {
   return tmp;
 }
 
+inline std::string ltrim(const std::string &s) {
+  std::string copy = s;
+  copy.erase(copy.cbegin(),
+             std::find_if(copy.cbegin(), copy.cend(),
+                          [](const char &c) { return !std::isspace(c); }));
+  return copy;
+}
+
+inline std::string rtrim(const std::string &s) {
+  std::string copy = s;
+  copy.erase(std::find_if(copy.crbegin(), copy.crend(),
+                          [](const char &c) { return !std::isspace(c); })
+                 .base(),
+             copy.end());
+  return copy;
+}
+
+inline std::string trim(const std::string &s) {
+  std::string copy = s;
+  copy = ltrim(copy);
+  copy = rtrim(copy);
+  return copy;
+}
 } // namespace strutil
 
 #endif // STRUTIL_HPP
