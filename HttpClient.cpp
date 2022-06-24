e4e7707fa6e292065fa9e5f6816c49d424805d2e
WilsonOh <oh.wilson123@gmail.com>
2022-06-24 15:53:18 +0800

    added comments to the source code

--- HttpClient.cpp
+++ HttpClient.cpp
@@ -12,6 +12,12 @@
 #include <openssl/tls1.h>
 #include <unistd.h>
 
+#ifdef VERBOSE
+static bool verbose = true;
+#else
+static bool verbose = false;
+#endif
+
 /*----------Constructor, Destructor and Setup Functions----------*/
 HttpClient::HttpClient(const url::Url &url) : _url(url) {}
 
@@ -56,8 +62,8 @@ void HttpClient::ssl_setup() {
   }
   std::string domain_with_port =
       fmt::format("{}:{}", _url.domain(), std::to_string(_url.port()));
-  // VERY IMPT ↓!! It sets the SNI for the host server which is mandatory for some
-  // TLS connections with some servers
+  // VERY IMPT ↓!! It sets the SNI for the host server which is mandatory for
+  // some TLS connections with some servers
   SSL_set_tlsext_host_name(ssl, _url.domain().c_str());
   BIO_set_conn_hostname(_bio, domain_with_port.c_str());
   if (BIO_do_connect(_bio) != 1) {
@@ -159,10 +165,18 @@ std::string HttpClient::get_formatted_request() {
 }
 
 /*---------------------"Engine" Methods-----------------*/
+
+/**
+ * Read and parse the response headers sent by the server
+ * and returns a pair of headers and the status code
+ */
 std::pair<std::map<std::string, std::string>, int>
 HttpClient::get_resp_headers() {
   std::string resp;
   char buf[2] = {0};
+  /* Read byte-by-byte from the server socket until we reach the
+   * end of the headers indicated by a "\r\n\r\n"
+   */
   while (read(_sockfd, buf, 1) > 0) {
     resp.append(buf);
     if (resp.find("\r\n\r\n") != std::string::npos) {
@@ -170,20 +184,35 @@ HttpClient::get_resp_headers() {
     }
   }
   std::vector<std::string> resp_headers = strutil::split(resp, "\n");
+  /* Get the iterator pointing to the first line */
   auto it = resp_headers.cbegin();
+  /* Since the status line has the format of:
+   * <METHOD STATUS_CODE STATUS_MSG>,
+   * we can get the status code by splitting the line on whitespace
+   * and taking the second word
+   */
   int statuscode = std::stoi(strutil::split(*it, " ")[1]);
+  /* Advance the iterator to the next line */
   it++;
+  /* Iterate over the rest of the lines and add them as headers*/
   std::map<std::string, std::string> ret;
   for (; it != resp_headers.cend(); ++it) {
     std::vector<std::string> tmp = strutil::split(strutil::trim(*it), ": ");
+    /* Sanity checks to make sure we don't do anything with empty strings */
     if (tmp.empty())
       continue;
     if (!tmp[0].empty() && !tmp[1].empty())
+      /* make the headers all lower-case for consistency thorughout the code
+       * base */
       ret.insert({strutil::lowers(tmp[0]), strutil::lowers(tmp[1])});
   }
   return {ret, statuscode};
 }
 
+/**
+ * Identical to `get_resp_headers` above, except we are using
+ * `BIO_read` instead of `read`
+ */
 std::pair<std::map<std::string, std::string>, int>
 HttpClient::ssl_get_resp_headers() {
   std::string resp;
@@ -209,6 +238,10 @@ HttpClient::ssl_get_resp_headers() {
   return {ret, statuscode};
 }
 
+/**
+ * Read a fixed number of bytes from the server using
+ * the `Content-Length` header
+ */
 std::string HttpClient::read_fixed_length_body(int length) {
   int total = 0;
   char buf[2] = {0};
@@ -220,6 +253,11 @@ std::string HttpClient::read_fixed_length_body(int length) {
   return body;
 }
 
+/**
+ * Read the the response body from the server in chunks
+ * if `Transfer-Encoding: chunked` is specified in the
+ * response headers.
+ */
 std::string HttpClient::read_chunked_body() {
   std::string body;
   char buf[2] = {0};
@@ -227,19 +265,41 @@ std::string HttpClient::read_chunked_body() {
   std::string::size_type n;
   int len;
   while (read(_sockfd, buf, 1)) {
+    /* Read into temp buffer until we hit "\r\n",
+     * indicating the start of a chunk
+     */
     tmp.append(buf);
     if ((n = tmp.find("\r\n")) != std::string::npos) {
+      /*
+       * If we reach this block, it means that the chunk size + "\r\n"
+       * is read into the temp buffer. In order the get the chunk size as a
+       * decimal number, we must remove the "\r\n" (can use strutil::trim
+       * instead)
+       */
       tmp.erase(n, tmp.length());
       len = std::stoi(tmp, 0, 16);
+      /* If len is 0, that means it is the end of the response body. So we
+       * return the body.
+       */
       if (len == 0)
         return body;
+      /**
+       * Else we can use the `read_fixed_length_body` to read `len` number of
+       * bytes from the server. IMPT: we have to read in `len + 2` bytes so as
+       * to take into account the "\r\n" at the end of each chunk!
+       */
       body.append(read_fixed_length_body(len + 2));
+      /* Clear the temp buffer for the next iteration */
       tmp.erase();
     }
   }
   return body;
 }
 
+/**
+ * Identical to its non_ssl counterpart, except this used `BIO_read`
+ * instead of `read`.
+ */
 std::string HttpClient::ssl_read_fixed_length_body(int length) {
   if (length == 0) {
     return "";
@@ -254,6 +314,10 @@ std::string HttpClient::ssl_read_fixed_length_body(int length) {
   return body;
 }
 
+/**
+ * Identical to its non_ssl counterpart, except this used `BIO_read`
+ * instead of `read`.
+ */
 std::string HttpClient::ssl_read_chunked_body() {
   std::string body;
   char buf[2] = {0};
@@ -303,27 +367,42 @@ HttpReponse HttpClient::ssl_send() {
 }
 
 HttpReponse HttpClient::non_ssl_send() {
+  /* First we have to setup the socket connection  */
   non_ssl_setup();
+  /* We then get the fully configured and formatted request string */
   std::string req = get_formatted_request();
+  /* We write the request to the server */
   if (write(_sockfd, req.c_str(), req.length()) == -1) {
     throw std::runtime_error("failed to write (non-ssl)");
   }
   auto [resp_headers, statuscode] = get_resp_headers();
   std::string body;
+  /* If the server responds with a "Content-Length" header, we will use the
+   * fixed-length method */
   if (resp_headers.find("content-length") != resp_headers.end()) {
     body = read_fixed_length_body(std::stoi(resp_headers.at("content-length")));
+    /* Else if "Transfer-Encoding" was specified instead, read from the server
+     * in chunks */
   } else if (resp_headers.find("transfer-encoding") != resp_headers.end()) {
     body = read_chunked_body();
+    /* Else panic as we don't know how to deal with any other methods of reading
+     * the body */
   } else {
     throw std::runtime_error("host server response header has no "
                              "content-length or chunked encoding");
   }
+  /* Check for redirection status codes */
   if (statuscode >= 300 && statuscode < 400) {
     std::string new_location = resp_headers.at("location");
+    /* If the server redirects to a relative path, construct the full path from
+     * the relative path */
     if (new_location[0] == '/') {
       new_location =
           fmt::format("{}://{}{}", _url.scheme(), _url.domain(), new_location);
     }
+    /* Create a new client and send a request to the new location.
+     * This can recursively redirect until we get an valid status code
+     */
     return new_client(new_location).send();
   }
   return {resp_headers, statuscode, body};
@@ -331,6 +410,10 @@ HttpReponse HttpClient::non_ssl_send() {
 
 HttpReponse HttpClient::send() {
   HttpReponse ret;
+  if (verbose) {
+    fmt::print("[LOG] Sending Request String:\n {}\n", get_formatted_request());
+  }
+  /* Choose between ssl and non-ssl methods depending on the scheme */
   if (_url.scheme() == "https") {
     ret = ssl_send();
   } else {
